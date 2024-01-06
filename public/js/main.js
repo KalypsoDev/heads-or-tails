f5fb81a4faf7834c4525278073bccfcf9d435a07
Angela <antunezangela89@gmail.com>
2024-01-06 12:58:04 +0100

    function updateScoreboard

--- public/js/main.js
+++ public/js/main.js
@@ -7,6 +7,19 @@ function flipCoin() {
     const coinSides = ["heads", "tails"];
     const randomSide = Math.floor(Math.random()*2);
     const outcome = coinSides[randomSide];
+    updateScoreboard(outcome);
+}
+
+function updateScoreboard(outcome) {
+    state[outcome]++;
+    const scoreBoard = document.getElementById('scoreboard-container');
+    const outcomeMessage = document.getElementById('outcome');
+    outcomeMessage.innerHTML = `You've got ${outcome}!`;
+    scoreBoard.classList.remove('hidden');
+    const headsUpdate = document.getElementById('heads_count');
+    headsUpdate.innerHTML = state.heads;
+    const tailsUpdate = document.getElementById('tails_count');
+    tailsUpdate.innerHTML = state.tails;
 }
 
 const resetButton = document.getElementById('reset');
@@ -15,5 +28,3 @@ resetButton.addEventListener('click', resetGame);
 function resetGame() {
     console.log('The game has started again');
 }
-
-
