e05bb86f85fa1ad24d225f3f1073790b67a11ed5
Angela <antunezangela89@gmail.com>
2024-01-06 02:13:25 +0100

    Add event listeners to flip and reset buttons

---
+++ main.js
@@ -0,0 +1,14 @@
+const flipButton = document.getElementById('flip');
+flipButton.addEventListener('click', flipCoin);
+
+function flipCoin() {
+    console.log('The coin is flipping');
+}
+
+const resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', resetGame);
+
+function resetGame() {
+    console.log('The game has started again');
+}
+
